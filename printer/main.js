01c3545239d27c42aec1516bd902f0809c87c81c
Zaneta <zanetawadrzyk@gmail.com>
2022-08-23 17:32:08 +0200

    printer project fix

--- printer/main.js
+++ printer/main.js
@@ -1,21 +1,34 @@
-
-window.addEventListener('load', function () {
-/**
- * @type {HTMLInputElement|null}
- */
-
-    const title = document.querySelector("#body");
-  /**
-  * @type {HTMLInputElement|null}
-  */
-
-    title?.addEventListener('keyup', function () {
-        // console.log('keyup' title.value);
-        if(titleCopy) {
-            titleCopy.value = 
-        }
+window.addEventListener("load", function () {
+    /**
+     * @type {HTMLInputElement|null}
+     */
+    const title = document.querySelector("#title");
+    /**
+     * @type {HTMLTextAreaElement|null}
+     */
+    const body = document.querySelector("#body");
+    /**
+     * @type {HTMLInputElement|null}
+     */
+    const titleCopy = document.querySelector("#title-copy");
+    /**
+     * @type {HTMLTextAreaElement|null}
+     */
+    const bodyCopy = document.querySelector("#body-copy");
+  
+    function setupPrinterFor(elementCopy, element) {
+      if (elementCopy) {
+        elementCopy.value = element.value;
+      } else {
+        console.log(elementCopy);
+      }
+    }
+    
+    title?.addEventListener("keyup", function () {
+        setupPrinterFor(titleCopy, title);
+      });
+      body?.addEventListener("keyup", function () {
+        setupPrinterFor(bodyCopy, body);
+      });
+      
     });
-    // title?.addEventListener('keypress', function () {
-    //     console.log('keypress' title.value);
-    // });
-})
